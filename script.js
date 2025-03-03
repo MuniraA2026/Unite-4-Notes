78baa4b4ce986fbaa95facd2953cc34404d55b5c
MuniraA2026 <munira.alkozai2026@gbwl.org>
2025-03-03 18:41:31 +0000

    added important notes

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+/* This is a MULTI-LINE comment 
+The code below is an example STATEMENT
+(instruction/command)
+console.log is a FUNCTION that displays info 
+in the console (output) area
+STATEMENT end with a SEMICOLON (;)
+*/
+console.log("Hey dude what's up?");
+
+// This is a SINGLE-LINE comment 
+
+// DECLARE a new VARIABLE
+let pokemon
+
+// ASSIGN a VALUE to the variable 
+//it's like puting an item in the box
+pokemon = "Pikachu"
