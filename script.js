1abf9f8c2444c78b3064e8e7ec255e15e7e51269
MuniraA2026 <munira.alkozai2026@gbwl.org>
2025-03-06 18:37:46 +0000

    added nail hahah

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ let pokemon
 // ASSIGN a VALUE to the variable 
 //it's like puting an item in the box
 pokemon = "Pikachu"
+
+// DECLARE + ASSIGN in one statement 
+let luckyNumber = 4;
+
+// "PASS IN" a variable to a function
+console.log(luckyNumber);
+
+// CHANGE the value of an existing variable
+let raining = false;
+console .log (raining);
+raining = true;
+console.log(raining);
+
+
+// COPY data from one variable into another
+let breakfast = "water"
+let lunch;
+lunch = breakfast;
+concole.log (breakfast);
+concole.log (lunch);
